d646cd7bd2b74a71eba024c5d9a35a3417beea43
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2019-07-04 18:21:15 +0200

    Add InformationCollector calls

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -28,12 +28,12 @@ void execute_runs(std::string experiment_config_path, std::string instance_name)
             ea.make_initial_individual(instance);
             for(int generation = 0; generation < experiment_config.generation_count; ++generation) {
                 ea.next_generation();
-                // information_collector->generation_snapshot(ea);
+                information_collector->generation_snapshot(ea);
             }
         }
     }
 
-    // information_collector->output_to_stream(std::cout);
+    information_collector->output_to_stream(std::cout);
 }
 
 }
